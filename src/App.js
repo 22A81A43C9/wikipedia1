e9009f127c5fec038431b9d037dc0f383d47b3d9
Satish <satishveeravalli2004@gmail.com>
2025-11-03 16:13:22 +0530

    Fixed homepage path for Vercel deployment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import Counter from './components/Counter';
 import Navbar from './components/Navbar';
