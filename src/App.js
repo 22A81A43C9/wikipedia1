03519651fb31c3693680682ca887a541ebcc0d03
Satish <satishveeravalli2004@gmail.com>
2025-11-03 15:31:08 +0530

    Fixed blank page issue by adding root route

--- src/App.js
+++ src/App.js
@@ -2,23 +2,20 @@ import React from 'react';
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import Counter from './components/Counter';
-
-import Navbar from './components/Navbar'; 
+import Navbar from './components/Navbar';
 import Wiki from './components/wiki';
-
-
-     
-
 import './App.css';
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* 👇 Default route for "/" */}
+        <Route path="/" element={<Wiki />} />
 
-        <Route exact path="/nav" element={<Navbar />} /> 
-         <Route exact path="/home" element={<Homepage/>} />
-         <Route path="/counter" element={<Counter />} />
+        <Route path="/nav" element={<Navbar />} />
+        <Route path="/home" element={<Homepage />} />
+        <Route path="/counter" element={<Counter />} />
         <Route path="/wikipedia" element={<Wiki />} />
       </Routes>
     </Router>
